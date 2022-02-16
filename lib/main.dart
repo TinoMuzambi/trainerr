2ee41161658fde4797ed0cbf2053e8b7a2353cf8
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-16 19:34:33 +0200

    Links new pages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:trainerr/pages/home.dart';
+import 'package:trainerr/pages/schedule.dart';
+import 'package:trainerr/pages/tracking.dart';
+import 'package:trainerr/pages/upcoming.dart';
 
 void main() {
 
   runApp(MaterialApp(
     initialRoute: "/",
     routes: {
-      "/": (context) => Home()
+      "/": (context) => Home(),
+      "/tracking": (context) => Tracking(),
+      "/upcoming": (context) => Upcoming(),
+      "/schedule": (context) => Schedule()
     },
   ));
 }
