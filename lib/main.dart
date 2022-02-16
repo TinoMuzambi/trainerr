646e90e5024d08aaf98d4620826ea36eb53d3f6b
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-16 17:15:05 +0200

    Abstracts colours into custom class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trainerr/utils/custom_colour.dart';
 
 void main() {
 
@@ -8,80 +9,60 @@ void main() {
 }
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    Map<int, Color> primaryColorShades = {
-      50: const Color.fromRGBO(13, 18, 29, .1),
-      100: const Color.fromRGBO(13, 18, 29, .2),
-      200: const Color.fromRGBO(13, 18, 29, .3),
-      300: const Color.fromRGBO(13, 18, 29, .4),
-      400: const Color.fromRGBO(13, 18, 29, .5),
-      500: const Color.fromRGBO(13, 18, 29, .6),
-      600: const Color.fromRGBO(13, 18, 29, .7),
-      700: const Color.fromRGBO(13, 18, 29, .8),
-      800: const Color.fromRGBO(13, 18, 29, .9),
-      900: const Color.fromRGBO(13, 18, 29, 1),
-    };Map<int, Color> accentColorShades = {
-      50: const Color.fromRGBO(149, 254, 106, .1),
-      100: const Color.fromRGBO(149, 254, 106, .2),
-      200: const Color.fromRGBO(149, 254, 106, .3),
-      300: const Color.fromRGBO(149, 254, 106, .4),
-      400: const Color.fromRGBO(149, 254, 106, .5),
-      500: const Color.fromRGBO(149, 254, 106, .6),
-      600: const Color.fromRGBO(149, 254, 106, .7),
-      700: const Color.fromRGBO(149, 254, 106, .8),
-      800: const Color.fromRGBO(149, 254, 106, .9),
-      900: const Color.fromRGBO(149, 254, 106, 1),
-    };
-    MaterialColor primaryColour = MaterialColor(0xff0d121d, primaryColorShades);
-    MaterialColor accentColour = MaterialColor(0xff95fe6a, accentColorShades);
+
+    CustomColour primaryColour = CustomColour(rgbColour: "0d121d");
+    CustomColour accentColour = CustomColour(rgbColour: "95fe6a");
 
     return Scaffold(
+      backgroundColor: primaryColour.getCustomColour(),
       appBar: AppBar(
         title: const Text("Trainerr"),
-        backgroundColor: primaryColour[900],
+        backgroundColor: primaryColour.getCustomColour()[400],
       ),
-      body:  Container(
-        color: primaryColour,
-        width: 500,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/logo-slim.png",
-              height: 200,
-              width: 100,
-            ),
-            TextButton(
-              child: Text(
-                  "I'm on a train",
-                style: TextStyle(
-                  color: accentColour,
-                ),
-              ),
-              onPressed: () {},
-            ),
-            TextButton(
-              child: Text(
-                "I'm about to get on a train",
-                style: TextStyle(
-                  color: accentColour,
-                ),
-              ),
-              onPressed: () {},
-            ),
-            TextButton(
-              child: Text(
-                "I just want the schedule",
-                style: TextStyle(
-                  color: accentColour,
-                ),
-              ),
-              onPressed: () {},
-            )
-          ],
-        ),
+      body:  Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.asset(
+            "assets/logo-slim.png",
+          ),
+          Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    child: Text(
+                      "I'm on a train",
+                      style: TextStyle(
+                        color: accentColour.getCustomColour()[900],
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                  TextButton(
+                    child: Text(
+                      "I'm about to get on a train",
+                      style: TextStyle(
+                        color: accentColour.getCustomColour()[900],
+                      ),
+                    ),
+                    onPressed: () {},
+                  ),
+                  TextButton(
+                    child: Text(
+                      "I just want the schedule",
+                      style: TextStyle(
+                        color: accentColour.getCustomColour()[900],
+                      ),
+                    ),
+                    onPressed: () {},
+                  )
+                ],
+              )
+          )
+        ],
       )
     );
   }
---
+++ lib/utils/custom_colour.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class CustomColour {
+  late String rgbColour;
+  late int R;
+  late int G;
+  late int B;
+  late Map<int, Color> shades;
+
+  CustomColour({ required this.rgbColour });
+
+  void hexToRGB() {
+    R = int.parse(rgbColour.substring(0, 2), radix: 16);
+    G = int.parse(rgbColour.substring(2, 4), radix: 16);
+    B = int.parse(rgbColour.substring(4, 6), radix: 16);
+
+    shades = {
+      50:  Color.fromRGBO(R, G, B, .1),
+      100:  Color.fromRGBO(R, G, B, .2),
+      200:  Color.fromRGBO(R, G, B, .3),
+      300:  Color.fromRGBO(R, G, B, .4),
+      400:  Color.fromRGBO(R, G, B, .5),
+      500:  Color.fromRGBO(R, G, B, .6),
+      600:  Color.fromRGBO(R, G, B, .7),
+      700:  Color.fromRGBO(R, G, B, .8),
+      800:  Color.fromRGBO(R, G, B, .9),
+      900:  Color.fromRGBO(R, G, B, 1),
+    };
+  }
+
+
+
+  MaterialColor getCustomColour() {
+    hexToRGB();
+    String hex = "0xff$R$G$B";
+    int hexNum = int.parse(hex);
+    return MaterialColor(hexNum, shades);
+  }
+}
