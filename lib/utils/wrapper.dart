6cf929a6d110a4d2d127e746f7b40f9aacd6c9b2
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-16 19:52:18 +0200

    Adds constructor.

--- lib/utils/wrapper.dart
+++ lib/utils/wrapper.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:trainerr/utils/custom_colour.dart';
 
 class Wrapper extends StatelessWidget {
-  Wrapper({Key? key}) : super(key: key);
-
   final CustomColour primaryColour = CustomColour(rgbColour: "0d121d");
   late final String text;
   late final Widget body;
+
+  Wrapper({Key? key,  required this.text, required this.body }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
