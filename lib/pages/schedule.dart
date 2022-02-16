4d2986c2574a4db72c00acc6154acb795f002737
Tino Muzambi <tinomuzambi@gmail.com>
2022-02-16 19:33:40 +0200

    Adds new pages.

---
+++ lib/pages/schedule.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Schedule extends StatefulWidget {
+  const Schedule({Key? key}) : super(key: key);
+
+  @override
+  _ScheduleState createState() => _ScheduleState();
+}
+
+class _ScheduleState extends State<Schedule> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
