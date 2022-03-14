a6203131e81cfb9f84d6db25ba06d1c7f5ca76dc
Tino Muzambi <tinomuzambi@gmail.com>
2022-03-14 21:52:30 +0200

    Adds api response class.

---
+++ lib/classes/trainerr_api_response.dart
@@ -0,0 +1,22 @@
+import 'package:trainerr/classes/train_route.dart';
+
+class TrainerrApiResponse {
+  final bool success;
+  final int page;
+  final List<TrainRoute> routes;
+  final int pages;
+
+  const TrainerrApiResponse(
+      {required this.success,
+      required this.page,
+      required this.routes,
+      required this.pages});
+
+  factory TrainerrApiResponse.fromMap(Map<String, dynamic> json) {
+    return TrainerrApiResponse(
+        success: json["success"],
+        page: json["page"],
+        routes: json["routes"],
+        pages: json["pages"]);
+  }
+}
